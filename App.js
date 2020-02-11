5d146613737ce982bd11ef5dd3ffd9476c29dc38
ahmedkamel095 <ahmedkamel095@gmail.com>
2020-02-12 00:10:47 +0200

    Working on CartScreen

--- App.js
+++ App.js
@@ -1,17 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
+import { AppLoading } from "expo";
+import * as Font from "expo-font";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import productsReducer from "./src/store/reducers/products";
+import cartReducer from "./src/store/reducers/cart";
 import ShopNavigator from "./src/navigation/ShopNavigator";
 
 const rootReducer = combineReducers({
-  product: productsReducer
+  product: productsReducer,
+  cart: cartReducer
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf")
+  });
+};
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => {
+          setFontLoaded(true);
+        }}
+      />
+    );
+  }
   return (
     <Provider store={store}>
       <ShopNavigator />
--- src/navigation/ShopNavigator.js
+++ src/navigation/ShopNavigator.js
@@ -2,18 +2,24 @@ import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import { Platform } from "react-native";
 
-import ProductsOverviewScreens from "../screens/shop/ProductsOverviewScreen";
+import ProductsOverviewScreen from "../screens/shop/ProductsOverviewScreen";
+import ProductDetailScreen from "../screens/shop/ProductDetialScreen";
+import CartScreen from "../screens/shop/CartScreen";
 import Colors from "../constants/Colors";
 
 const ProductsNavigator = createStackNavigator(
   {
-    ProductsOverview: ProductsOverviewScreens
+    ProductsOverview: ProductsOverviewScreen,
+    ProductDetail: ProductDetailScreen,
+    Cart: CartScreen
   },
   {
     defaultNavigationOptions: {
       headerStyle: {
         backgroundColor: Platform.OS === "android" ? Colors.primary : ""
       },
+      headerTitleStyle: { fontFamily: "open-sans-bold" },
+      headerBackTitleStyle: { fontFamily: "open-sans" },
       headerTintColor: Platform.OS === "android" ? "white" : Colors.primary
     }
   }
